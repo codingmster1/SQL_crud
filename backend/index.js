6cae5f261e8299ed968132951bbf5527c6330e34
Jeffrey Doyle <doylejeff98@gmail.com>
2025-01-15 12:21:39 -0500

    created repo for crud app sql node react

---
+++ backend/index.js
@@ -0,0 +1,89 @@
+import express from 'express';
+import mysql from 'mysql2';
+import cors from 'cors';
+const app = express();
+
+const db = mysql.createConnection({
+host: 'localhost',
+user: 'root',
+port: '3306',
+password: "password789",
+database: 'new_schema'
+})
+
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {    
+/* res.json("hello from the backend") */
+})
+
+
+app.get("/books", (req, res) => {
+const q = 'SELECT * FROM books'
+db.query(q,(err, data) => {
+if (err) {
+    return res.json(err)
+} else {
+return res.json(data)
+}
+})
+})
+
+app.post("/books", (req, res) => {
+const q = "INSERT INTO books (`title`, `desc`, `price`, `cover`) VALUES (?)"
+const values =[
+    req.body.title,
+    req.body.desc,
+    req.body.price,
+    req.body.cover
+]
+
+
+db.query(q, [values], (err, data) => {
+    if (err) {
+        return res.json(err)
+    } else {
+    return res.json("Books added successfully")
+    }
+})
+})
+
+app.delete("/books/:id", (req, res) => {
+const bookId = req.params.id;
+const q = "DELETE FROM books WHERE id = ?"
+
+db.query(q, [bookId], (err, data) => {
+if (err) {
+    return res.json(err)
+} else {
+    return res.json("Books deleted successfully")
+    }
+})
+})
+
+
+app.put("/books/:id", (req, res) => {
+    const bookId = req.params.id;
+    const q = "UPDATE books SET `title` = ?, `desc` = ?, `price` = ?, `cover` = ? WHERE id = ?"
+    
+const values =[
+    req.body.title,
+    req.body.desc,
+    req.body.price,
+    req.body.cover
+]
+
+    db.query(q, [...values, bookId], (err, data) => {
+    if (err) {
+        return res.json(err)
+    } else {
+        return res.json("Books updated successfully")
+        }
+    })
+    })
+
+app.listen(8000, () => {
+console.log('Server is running on port 8000');    
+})
